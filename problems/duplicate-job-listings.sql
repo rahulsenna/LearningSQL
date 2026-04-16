420c3ef80dde1f54a0b1352f72950f872462d9b0
Rahul Senna <rahulvsenna@gmail.com>
2026-04-16 14:58:46 +0530

    duplicate-job-listings

---
+++ problems/duplicate-job-listings.sql
@@ -0,0 +1,8 @@
+-- https://datalemur.com/questions/duplicate-job-listings
+SELECT COUNT(DISTINCT company_id) AS duplicate_companies
+FROM (
+  SELECT *
+  FROM job_listings
+  GROUP BY company_id, title, description
+  HAVING COUNT(*) > 1
+) t;
